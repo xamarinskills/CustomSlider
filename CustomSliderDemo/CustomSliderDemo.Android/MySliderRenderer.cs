ca5ed6cab2b88c714c5137ae7487921e5c418375
sumitsisodia.xamarinskills@gmail.com <sumitsisodia@elensoft.com>
2017-12-23 18:15:00 +0530

    Add project files.

---
+++ CustomSliderDemo/CustomSliderDemo.Android/MySliderRenderer.cs
@@ -0,0 +1,68 @@
+﻿using Android.Widget;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using System.ComponentModel;
+using Android.Graphics;
+using CustomSliderDemo;
+using CustomSliderDemo.Droid;
+
+[assembly: ExportRenderer(typeof(CustomSlider), typeof(MySliderRenderer))]
+namespace CustomSliderDemo.Droid
+{
+    public class MySliderRenderer : SliderRenderer
+    {
+        private CustomSlider view;
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Slider> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement != null || e.NewElement == null)
+                return;
+
+            view = (CustomSlider)Element;
+            if (!string.IsNullOrEmpty(view.ThumbImage))
+            {
+                // Set Thumb Icon
+                Control.SetThumb(Resources.GetDrawable(view.ThumbImage));
+            }
+            else if (view.ThumbColor != Xamarin.Forms.Color.Default ||
+                view.MaxColor != Xamarin.Forms.Color.Default ||
+                view.MinColor != Xamarin.Forms.Color.Default)
+
+                Control.Thumb.SetColorFilter(view.ThumbColor.ToAndroid(), PorterDuff.Mode.SrcIn);
+                Control.ProgressTintList = Android.Content.Res.ColorStateList.ValueOf(view.MinColor.ToAndroid());
+                Control.ProgressTintMode = PorterDuff.Mode.SrcIn;
+                //this is for Maximum Slider line Color
+                Control.ProgressBackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(view.MaxColor.ToAndroid());
+                Control.ProgressBackgroundTintMode = PorterDuff.Mode.SrcIn;
+            
+        }
+
+
+
+        protected override void OnLayout(bool changed, int l, int t, int r, int b)
+        {
+            base.OnLayout(changed, l, t, r, b);
+
+            if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.JellyBean)
+            {
+                if (Control == null)
+                {
+                    return;
+                }
+
+                SeekBar ctrl = Control;
+                Drawable thumb = ctrl.Thumb;
+                int thumbTop = ctrl.Height / 2 - thumb.IntrinsicHeight / 2;
+
+                thumb.SetBounds(thumb.Bounds.Left, thumbTop,
+                                thumb.Bounds.Left + thumb.IntrinsicWidth, thumbTop + thumb.IntrinsicHeight);
+            }
+        }
+
+      
+
+    }
+
+}
---
+++ CustomSliderDemo/CustomSliderDemo.iOS/MySliderRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using CustomSliderDemo;
+using CustomSliderDemo.iOS;
+using Xamarin.Forms.Platform.iOS;
+using System.ComponentModel;
+
+[assembly: ExportRenderer(typeof(CustomSlider), typeof(MySliderRenderer))]
+namespace CustomSliderDemo.iOS
+{
+    public class MySliderRenderer : SliderRenderer
+    {
+        private CustomSlider view;
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Slider> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement != null || e.NewElement == null)
+                return;
+
+            view = (CustomSlider)Element;
+            if (!string.IsNullOrEmpty(view.ThumbImage))
+            {
+                //Assigns a thumb image to the specified control states.
+                Control.SetThumbImage(UIImage.FromFile(view.ThumbImage), UIControlState.Normal);
+            }
+            else if (view.ThumbColor != Xamarin.Forms.Color.Default ||
+                view.MaxColor != Xamarin.Forms.Color.Default ||
+                view.MinColor != Xamarin.Forms.Color.Default)
+                // Set Progress bar Thumb color
+                Control.ThumbTintColor = view.ThumbColor.ToUIColor();
+            //this is for Minimum Slider line Color
+            Control.MinimumTrackTintColor = view.MinColor.ToUIColor();
+            //this is for Maximum Slider line Color
+            Control.MaximumTrackTintColor = view.MaxColor.ToUIColor();
+
+        }
+    }
+}
